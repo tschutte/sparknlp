020401beb68c85ac94f356326f19480434e44763
Terry Schutte <tschutte@gmail.com>
2021-03-04 10:40:00 -0600

    Update dependencies.R

--- R/dependencies.R
+++ R/dependencies.R
@@ -21,15 +21,15 @@ spark_dependencies <- function(spark_version, scala_version, ...) {
     
     sparklyr::spark_dependency(
       jars = c(
-        #system.file(
-        #  sprintf("java/sparknlp-%s-%s.jar", spark_version, scala_version),
-        #  package = "sparknlp"
-        #),
+        system.file(
+          sprintf("java/sparknlp-%s-%s.jar", spark_version, scala_version),
+          package = "sparknlp"
+        ),
         jsl_url
-      ),
-      packages = c(
-        sprintf("com.johnsnowlabs.nlp:spark-nlp_2.11:%s", spark_nlp_version)
-      )
+      )#,
+      #packages = c(
+      #  sprintf("com.johnsnowlabs.nlp:spark-nlp_2.11:%s", spark_nlp_version)
+      #)
     )  
   }
 }
